88c27247c25b4ebddfe7bd849af71a215ca7577c
nouhailaelmir <nouhailaelmir4@gmail.com>
2023-08-30 16:27:27 -0700

    commit

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,71 +1,40 @@
-/*
- * File: 103-python.c
- * Auth: Brennan D Baraban
- */
+#!/usr/bin/python3
+"""Defines a class MagicClass"""
+import math
 
-#include <Python.h>
 
-void print_python_list(PyObject *p);
-void print_python_bytes(PyObject *p);
+class MagicClass:
+    """
+    Class that defines properties of MagicClass.
 
-/**
- * print_python_list - Prints basic info about Python lists.
- * @p: A PyObject list object.
- */
-void print_python_list(PyObject *p)
-{
-	int size, alloc, i;
-	const char *type;
-	PyListObject *list = (PyListObject *)p;
-	PyVarObject *var = (PyVarObject *)p;
+    Attributes:
+        radius: radius.
+    """
+    def __init__(self, radius=0):
+        """Creates new instances of MagicClass.
 
-	size = var->ob_size;
-	alloc = list->allocated;
+        Args:
+            radius: radius.
 
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %d\n", size);
-	printf("[*] Allocated = %d\n", alloc);
+        Raises:
+            TypeError: radius must be a number .
+        """
+        self.__radius = 0
+        if not isinstance(radius, int) and not isinstance(radius, float):
+            raise TypeError("radius must be a number")
+        else:
+            self.__radius = radius
 
-	for (i = 0; i < size; i++)
-	{
-		type = list->ob_item[i]->ob_type->tp_name;
-		printf("Element %d: %s\n", i, type);
-		if (strcmp(type, "bytes") == 0)
-			print_python_bytes(list->ob_item[i]);
-	}
-}
+    def area(self):
+        """Returns area
 
-/**
- * print_python_bytes - Prints basic info about Python byte objects.
- * @p: A PyObject byte object.
- */
-void print_python_bytes(PyObject *p)
-{
-	unsigned char i, size;
-	PyBytesObject *bytes = (PyBytesObject *)p;
+        Returns: area.
+        """
+        return math.pi * self.__radius ** 2
 
-	printf("[.] bytes object info\n");
-	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
-	{
-		printf("  [ERROR] Invalid Bytes Object\n");
-		return;
-	}
+    def circumference(self):
+        """Returns circumference
 
-	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
-	printf("  trying string: %s\n", bytes->ob_sval);
-
-	if (((PyVarObject *)p)->ob_size > 10)
-		size = 10;
-	else
-		size = ((PyVarObject *)p)->ob_size + 1;
-
-	printf("  first %d bytes: ", size);
-	for (i = 0; i < size; i++)
-	{
-		printf("%02hhx", bytes->ob_sval[i]);
-		if (i == (size - 1))
-			printf("\n");
-		else
-			printf(" ");
-	}
-}
+        Returns: circumference.
+        """
+        return 2 * math.pi * self.__radius
